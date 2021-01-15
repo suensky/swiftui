44b7febf5a1cb4cc9288f637c37c14418a1c36e4
suensky <suensky@outlook.com>
2021-01-15 15:18:04 -0800

    SwiftUI: Views, Stacks and Modifiers

---
+++ scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Huanhuan Sun on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
